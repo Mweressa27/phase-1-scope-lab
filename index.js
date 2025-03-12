42911de913be2d4fa2eddbe0761ec70f58c50bea
Mweresa Esther Magoma <esthermweresa27@gmail.com>
2025-03-12 09:07:55 +0300

    complete tests

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+var bestCustomer;
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+
+  var bestCustomer;
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'me';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'you';
+}
